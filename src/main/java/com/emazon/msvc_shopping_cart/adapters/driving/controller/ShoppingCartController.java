9f1e8d35fa9ab4897486e4453d000878766db6d0
Jorge <jomruizgo@gmail.com>
2024-09-16 14:42:34 -0500

    HU8: spring security and Jwt configured

--- src/main/java/com/emazon/msvc_shopping_cart/adapters/driving/controller/ShoppingCartController.java
+++ src/main/java/com/emazon/msvc_shopping_cart/adapters/driving/controller/ShoppingCartController.java
@@ -1,10 +1,19 @@
 package com.emazon.msvc_shopping_cart.adapters.driving.controller;
 
 import com.emazon.msvc_shopping_cart.domain.util.Constants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = Constants.API_SHOPPING_CART_PATH)
 public class ShoppingCartController {
+
+    @PostMapping("")
+    public ResponseEntity<String> sell(){
+        String regard = "Hello client user from sopping cart microservice.";
+        return new ResponseEntity<>(regard, HttpStatus.OK);
+    }
 }
